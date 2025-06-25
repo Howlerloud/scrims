426accf6f9ef7e2a03a34c0789021ff20271f30d
Howlerloud <howlerjr@hotmail.co.uk>
2025-06-25 16:57:52 +0100

    Added new model for users

--- scrim/admin.py
+++ scrim/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Post
-
+from .models import Usercreate
 
 # Register your models here.
 
 
 admin.site.register(Post)
+
+
+admin.site.register(Usercreate)
--- scrim/models.py
+++ scrim/models.py
@@ -16,3 +16,33 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+
+
+ACTIVE = ((0, "Playing"), (1, "Sub"))
+
+
+# Create your models here.
+class Usercreate(models.Model):
+    Username = models.TextField(max_length=30, unique=True)
+    RANK = [
+        ('bronze', 'Bronze'),
+        ('silver', 'Silver'),
+        ('gold', 'Gold'),
+        ('platinum', 'Platinum'),
+        ('diamond', 'Diamond'),
+        ('grandmaster', 'Grandmaster'),
+        ('clestial', 'Celestial'),
+        ('eternity', 'Eternity'),
+    ]
+
+    rank = models.CharField(max_length=200, choices=RANK, default='Bronze')
+
+    role = [
+        ('dps', 'DPS'),
+        ('support', 'Support'),
+        ('tank', 'Tank'),
+    ]
+    created_on = models.DateTimeField(auto_now_add=True)
+    teamstatus = models.IntegerField(choices=ACTIVE, default=1)
+
+
