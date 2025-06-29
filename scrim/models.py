0bfb2f5e58153b76a6f5941ad0f9d39a8cfc2881
Howlerloud <howlerjr@hotmail.co.uk>
2025-06-29 13:57:47 +0100

    added meta to models to sort by date

--- scrim/models.py
+++ scrim/models.py
@@ -18,7 +18,10 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title}"
 
 
 ACTIVE = ((0, "Playing"), (1, "Sub"))
@@ -32,6 +35,14 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.body[:20], f"by {self.author}" 
+    
+    
 
 
 # Create your models here.
