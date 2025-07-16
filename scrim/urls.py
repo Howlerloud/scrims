ef22e9307cf308fccfa2888ebeb83e565d356ac6
Howlerloud <howlerjr@hotmail.co.uk>
2025-07-16 18:07:52 +0100

    addedpost_detail view

--- scrim/urls.py
+++ scrim/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('find/', views.FindView.as_view(), name='find'),
     path('lfg/', views.LfgView.as_view(), name='lfg'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
 
 # path('admin/', admin.site.urls),
