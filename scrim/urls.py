6f9611fcb80d2968e0033321d7558467741fac35
Howlerloud <howlerjr@hotmail.co.uk>
2025-07-18 17:43:18 +0100

    Added login and logout page

--- scrim/urls.py
+++ scrim/urls.py
@@ -1,10 +1,11 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path("accounts/", include("allauth.urls")),
     path('find/', views.FindView.as_view(), name='find'),
     path('lfg/', views.LfgView.as_view(), name='lfg'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
